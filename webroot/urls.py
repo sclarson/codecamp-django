ad699a3ea5bed09d8d0d48ebaecb65943a661f10
sclarson <seth.larson@gmail.com>
2012-08-21 00:00:50 -0500

    Adds routes and views for speakers and sessions

--- webroot/urls.py
+++ webroot/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.simple import redirect_to
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 from codecamp.models import Speaker, Session
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -11,9 +13,17 @@ urlpatterns = patterns('',
     # url(r'^codecamp/', include('codecamp.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
+    
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    (r'^speakers$', redirect_to, {'url': '/speakers/'}),
+    url(r'^speakers/$', 'codecamp.views.speakers_index', name='speaker_index'),
+    url(r'^speakers/(?P<slug>[-\w\d]+),(?P<id>\d+)/$','codecamp.views.speaker_detail', name='speaker_detail'),
+    (r'^sessions$', redirect_to, {'url': '/sessions/'}),
+    url(r'^sessions/$', 'codecamp.views.sessions_index', name='session_index'),
+    url(r'^sessions/(?P<slug>[-\w\d]+),(?P<id>\d+)/$','codecamp.views.session_detail', name='session_detail'),
     (r'', include('django.contrib.flatpages.urls')),
+    
 )
