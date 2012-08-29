7b626e4ba1a1d05bde5832c0901da2c1e3f30195
sclarson <seth.larson@gmail.com>
2012-08-29 16:44:30 -0500

    adds redirects for session submission and registration

--- webroot/urls.py
+++ webroot/urls.py
@@ -21,11 +21,13 @@ urlpatterns = patterns('',
     url(r'^sessions/$', 'codecamp.views.sessions_index'),
     (r'^sessions$', redirect_to, {'url': '/sessions/'}),
     url(r'^sessions/(?P<slug>[-\w\d]+),(?P<id>\d+)/$','codecamp.views.session_detail', name='session_detail'),
+    (r'^submit$', redirect_to, {'url':'/sessions/submit'}),
     url(r'^sessions/submit$', 'codecamp.views.session_submit', name='session_submit' ),
     # Speakers
     url(r'^speakers/$', 'codecamp.views.speakers_index'),
     (r'^speakers$', redirect_to, {'url': '/speakers/'}),
     url(r'^speakers/(?P<slug>[-\w\d]+),(?P<id>\d+)/$','codecamp.views.speaker_detail', name='speaker_detail'),
+    (r'register$',redirect_to, {'url': 'http://www.eventbrite.com/event/4216204782/es2?utm_source=sdcc_redir'}),
     # Flatpages
     (r'', include('django.contrib.flatpages.urls')),
 
