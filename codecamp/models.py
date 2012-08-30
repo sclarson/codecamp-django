1015f2eb3ca5ed1ee515ba3a5f3219f712b06889
sclarson <seth.larson@gmail.com>
2012-08-30 10:45:23 -0500

    media path updates

--- codecamp/models.py
+++ codecamp/models.py
@@ -17,7 +17,7 @@ class Speaker(models.Model):
     website = models.URLField(blank=True)
     company_website = models.URLField(blank=True)
     slug = models.SlugField(unique=True)
-    photo = models.ImageField(upload_to='media/images/speakers')
+    photo = models.ImageField(upload_to='images/speakers')
 
     class Meta:
         ordering = ['last_name']
