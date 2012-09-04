690c2aa304d2333fa435dbfffb19295a81a0f7f4
Tyler Harms <tyler.harms@gmail.com>
2012-09-04 16:03:08 -0500

    removes deprecated included.

--- codecamp/urls/speakers.py
+++ codecamp/urls/speakers.py
@@ -1,11 +1,9 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 
 from codecamp.models import Speaker
 
 
 urlpatterns = patterns('',
-                       url(r'^$', 'codecamp.views.speakers_index'),
-                       url(r'^(?P<slug>[-\w\d]+),(?P<id>\d+)/$',
-                           'codecamp.views.speaker_detail',
-                           name='speaker_detail'),
-                       )
+    url(r'^$', 'codecamp.views.speakers_index'),
+    url(r'^(?P<slug>[-\w\d]+),(?P<id>\d+)/$', 'codecamp.views.speaker_detail', name='speaker_detail'),
+)
