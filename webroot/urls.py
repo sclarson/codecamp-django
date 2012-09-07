f04ea7987f504f341812e5235decb30fe45d88f6
Tyler Harms <tyler.harms@gmail.com>
2012-09-06 19:55:57 -0500

    Adds a RSS Feed to code camp

--- webroot/urls.py
+++ webroot/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic.simple import redirect_to
+from codecamp.feeds import SessionsFeed
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -23,6 +24,7 @@ urlpatterns = patterns('',
     (r'sessions/', include('codecamp.urls.sessions')),
     (r'^submit$', redirect_to, {'url': '/sessions/submit'}),
     url(r'^sessions/submit$', 'codecamp.views.session_submit', name='session_submit'),
+    (r'^sessions/feed/$', SessionsFeed()),
     # Speakers
     (r'^speakers$', redirect_to, {'url': '/speakers/'}),
     (r'^speakers/', include('codecamp.urls.speakers')),
