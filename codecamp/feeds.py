55b499c97a4268691af20d445e0ba07c2464d718
Tyler Harms <tyler.harms@gmail.com>
2012-09-07 09:45:52 -0500

    Adds in an atom feed to go along with the rss2 feed.
    Adds feeds to header
    Adds some SEO to header

--- codecamp/feeds.py
+++ codecamp/feeds.py
@@ -1,9 +1,24 @@
-from django.utils.feedgenerator import Atom1Feed
+from django.utils.feedgenerator import Atom1Feed, Rss201rev2Feed
 from django.contrib.syndication.views import Feed
 from models import Session
 
-class SessionsFeed(Feed):
+class SessionsFeedAtom(Feed):
     feed_type = Atom1Feed
+    title = "South Dakota Codecamp Sessions"
+    link = "/sessions/"
+    description = "Contains the list os South Dakota Codecamp Sessions."
+
+    def items(self):
+        return Session.objects.order_by('title')
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.abstract
+
+class SessionsFeedRSS2(Feed):
+    feed_type = Rss201rev2Feed
     title = "South Dakota Codecamp Sessions"
     link = "/sessions/"
     description = "Contains the list os South Dakota Codecamp Sessions."
