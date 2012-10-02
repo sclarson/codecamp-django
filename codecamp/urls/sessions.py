06048d5e5fef780d31b66077db3347d812633d9a
sclarson <seth.larson@gmail.com>
2012-10-02 16:07:53 -0500

    Adds session year URLs

--- codecamp/urls/sessions.py
+++ codecamp/urls/sessions.py
@@ -5,5 +5,7 @@ from codecamp.models import Session
 
 urlpatterns = patterns('',
     url(r'^$', 'codecamp.views.sessions_index'),
+    url(r'^(?P<year>\d{4})/$','codecamp.views.session_archive', name='session_archive'),
+    url(r'^(?P<year>\d{4})/(?P<slug>[-\w\d]+),(?P<id>\d+)/$','codecamp.views.session_detail', name='session_slug'),
     url(r'^(?P<slug>[-\w\d]+),(?P<id>\d+)/$', 'codecamp.views.session_detail', name='session_detail'),
 )
